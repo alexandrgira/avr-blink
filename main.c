65ffe183e0f0a2e00808c5ccfcf914f45e5fc732
alexandrgira <alexandrgira@hotmail.com>
2025-12-09 23:57:13 +0300

    Minor changes to main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define BLINK_PERIOD 2000 // Period of one blink cycle (in ms)
 
 int main(void) {
     DDRB |= (1 << DDB5);
-
-    while (1) {
-        PORTB ^= (1 << 5);
-        _delay_ms(1000);
+    
+    while (true) {
+        PORTB ^= (1 << DDB5);
+        _delay_ms(BLINK_PERIOD / 2);
     }
 }
